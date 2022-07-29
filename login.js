0ba6b80d2ecf875827aca277e62822e88867f520
Safira Oktarini <92132110+safiraoktarini@users.noreply.github.com>
2022-07-29 15:06:01 +0700

    Add files via upload

---
+++ login.js
@@ -0,0 +1,42 @@
+//validasi untuk input login
+
+var username = document.forms['form']['username']
+var password = document.forms['form']['password']
+
+var username_error = document.getElementById('username_error')
+var password_error = document.getElementById('password_error')
+
+username.addEventListener('textInput', username_Verify);
+password.addEventListener('textInput', password_Verify);
+
+function validated(){
+    if (username.value.length > 6){
+        username.style.border = "1px solid red";
+        username_eror.style.display = "block";
+        username.focus();
+        return false;
+    }
+    if (password.value.length < 6){
+        password.style.border = "1px solid red";
+        password_eror.style.display = "block";
+        password.focus();
+        return false();
+    }
+
+}
+
+function username_Verify(){
+    if(username.value.length <= 6){
+        username.style.border = "1px solid silver";
+        username_eror.style.display = "none";
+        return true;
+    }
+}
+
+function password_Verify(){
+    if(password.value.length <= 6){
+        password.style.border = "1px solid silver";
+        password_eror.style.display = "none";
+        return true;
+    }
+}
